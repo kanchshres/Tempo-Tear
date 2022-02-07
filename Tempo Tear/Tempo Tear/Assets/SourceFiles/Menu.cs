99128d603e78cba49ecd9162e268ef7a92723516
KazSusilo <20ARS9@queensu.ca>
2022-02-06 22:36:56 -0500

    Menu Rework
    Title Change
    Developed on the new editor
    Fully functioning
    PlayButton sends to level select screen which can't be escaped

---
+++ Tempo Tear/Tempo Tear/Assets/SourceFiles/Menu.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Menu : MonoBehaviour
+{
+    // Resolution Variables
+    public TMPro.TMP_Dropdown resolutionsDropdown;
+    Resolution[] screenResolutions;
+    List<int> resolutionIndexes = new List<int>();
+
+    // Graphic Variables
+    public TMPro.TMP_Dropdown graphicsDropdown;
+
+    // Audio Variables
+    public AudioMixer audioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Resolutions setup
+        screenResolutions = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int count = 0;
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < screenResolutions.Length; i++)
+        {
+            if (screenResolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                string option = screenResolutions[i].width + " x " + screenResolutions[i].height;
+                options.Add(option);
+                resolutionIndexes.Add(i);
+
+                if (screenResolutions[i].width == Screen.currentResolution.width && 
+                    screenResolutions[i].height == Screen.currentResolution.height)
+                {
+                    currentResolutionIndex = count;
+                }
+                count++;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
+
+        // Graphics setup
+        QualitySettings.SetQualityLevel(2);
+        graphicsDropdown.RefreshShownValue();
+    }
+
+
+    // Moves to the "LevelSelect" scene
+    public void playGame()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+
+
+    // Set game resolution
+    public void setResolution(int resolutionIndex)
+    {
+        resolutionIndex = (int) resolutionIndexes[resolutionIndex];
+        Resolution resolution = screenResolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+
+    // Toggle fullscreen mode
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+
+    // Set graphics quality
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+
+    // Set volumes
+    public void setMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+    public void setMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+    public void setEffectsVolume(float volume)
+    {
+        audioMixer.SetFloat("EffectsVolume", volume);
+    }
+
+
+    // Quits application
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
