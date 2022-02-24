089ad473eb194749fc3c0f1c43d5b2601d24785d
Kanchan <shrestha.kanch@gmail.com>
2022-02-24 14:10:28 -0700

    Zombie Animations + Script

---
+++ Tempo Tear/Assets/SourceFiles/Enemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float beatTempo;
+
+    public bool hasStarted;
+
+    public bool goesRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beatTempo = beatTempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            if (Input.anyKeyDown)
+            {
+                hasStarted = true;
+            }
+        }
+        else
+        {
+            transform.position += new Vector3(beatTempo * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
