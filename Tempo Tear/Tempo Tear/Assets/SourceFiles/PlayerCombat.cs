1965bb6dcb5efb490a4a65a80b80fbb3c5d18684
KazSusilo <20ARS9@queensu.ca>
2022-02-24 15:03:04 -0500

    Samurai Animation and Script
    Samurai animations have been made
    
    Player_Combat script has been created
    * Need to determine if player is attacking
    * Need to determine if player is hit
    * Need to detect enemies corresponding to slash
    * * Damage those enemies

---
+++ Tempo Tear/Tempo Tear/Assets/SourceFiles/PlayerCombat.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    // Animator Variable
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Determine if player is attacking
+        if (Input.GetMouseButtonDown(0))
+        {
+            Slash();
+        }
+
+        // Determine if player is hit
+        if (Input.GetMouseButtonDown(1))
+        {
+            Hit();
+        }
+
+    }
+
+    // 
+    void Slash()
+    {
+        // Play an attack animation
+        animator.SetTrigger("Slash");
+
+    }
+
+    void Hit()
+    {
+        // Play a hit animation
+        animator.SetTrigger("Hit");
+    }
+}
